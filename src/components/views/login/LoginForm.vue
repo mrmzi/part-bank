56c2bc924433bf5ecbe4348f81eb6c9cc60e7d9b
mrmzi <rezamiryazdi051@gmail.com>
2025-09-25 20:38:19 +0330

    Add rotated background with opacity to login section

--- src/components/views/login/LoginForm.vue
+++ src/components/views/login/LoginForm.vue
@@ -1,9 +1,9 @@
 <script setup></script>
 <template>
-  <form class="login__form" action="">
+  <form class="login" dir="rtl">
     <div class="login__logo">
-      <img src="./images/Logo.svg" alt="لوگو پارت بانک" />
-      <div class="login__logo__header">
+      <img class="login__logo-image" src="../../../assets/images/login/Logo.svg" alt="لوگو پارت بانک" />
+      <div class="login__logo-header">
         <h1 class="login__logo-title">پارت بانک</h1>
         <span class="login__logo-subtitle">تجربه‌ای نوین در بانک داری</span>
       </div>
@@ -25,4 +25,101 @@
     </div>
   </form>
 </template>
-<style scoped></style>
+
+<style lang="scss" scoped>
+.login {
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 83.5px;
+
+  &__logo {
+    width: 279px;
+    height: 92px;
+    display: flex;
+
+    &-image {
+      width: auto;
+      height: auto;
+    }
+
+    &-header {
+      margin: 24px 16px 0 0;
+      width: 169px;
+      height: 68px;
+      color: rgba(65, 82, 160, 1);
+    }
+
+    &-title {
+      font-size: 40px;
+      font-family: 'peyda-bold';
+    }
+
+    &-subtitle {
+      font-size: 16px;
+    }
+  }
+
+  &__inputs {
+    display: flex;
+    flex-direction: column;
+    margin-top: 103.5px;
+    width: 354px;
+    height: 280px;
+  }
+
+  &__label {
+    width: 100%;
+    height: 22px;
+    font-size: 16px;
+    margin-bottom: 4px;
+  }
+
+  &__input {
+    width: 100%;
+    height: 48px;
+    background-color: #f9fafb;
+    border: 0;
+    border-radius: 8px;
+    margin-bottom: 8px;
+    padding: 14px 8px;
+    outline: none;
+    font-size: 14px;
+    direction: rtl;
+
+    &::placeholder {
+      color: #c3c5c9;
+    }
+  }
+
+  &__button {
+    font-family: 'peyda-bold';
+    width: 100%;
+    height: 48px;
+    background-color: #4152a0;
+    border-radius: 8px;
+    margin-top: 32px;
+    border: 0;
+    color: #ffffff;
+    font-size: 16px;
+    cursor: pointer;
+  }
+
+  &__support {
+    width: 143px;
+    height: 20px;
+    display: flex;
+    margin-bottom: 10px;
+    margin-top: 229px;
+
+    &-label {
+      margin-left: 4px;
+    }
+
+    &-number {
+      font-weight: bold;
+    }
+  }
+}
+</style>
--- src/components/views/login/LoginSection.vue
+++ src/components/views/login/LoginSection.vue
@@ -1,20 +1,21 @@
 <script setup>
-import LoginImage from './LoginImage.vue';
-import LoginForm from './LoginForm.vue';
+import LoginImage from './LoginImage.vue'
+import LoginForm from './LoginForm.vue'
 </script>
 <template>
-    <section class="login-container">
-        <LoginImage></LoginImage>
-        <LoginForm></LoginForm>
-    </section>
+  <section class="login-container">
+    <LoginImage></LoginImage>
+    <LoginForm></LoginForm>
+  </section>
 </template>
 <style scoped>
 .login-container {
-     width: 1200px;
-    height: 840px;
-    display: flex;
-    align-items: center;
-    padding: 20px;
-    background-color: #ffffff;
+  width: 1200px;
+  height: 840px;
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  background-color: #ffffff;
+  overflow: hidden;
 }
 </style>
